d24fdeec8d54b896bb9eb08bcfb19fb88c621c79
ahmedosama24 <mrahmed2466@gmail.com>
2023-09-25 03:06:34 +0300

    almost finished :Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-
+import {Route,Routes,Outlet,} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import './app.scss';
 function App() {
-
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
   return (
-   <>app</>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />}/>
+      </Route>
+    </Routes>
   )
 }
 
