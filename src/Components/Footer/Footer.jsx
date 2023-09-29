5fc6f0d470ba8474e9737a1e6adb755288c57246
ahmedosama24 <mrahmed2466@gmail.com>
2023-09-30 00:18:57 +0300

    halfFooter

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='Footer'>
+      <div className="container">
+        <div className="top">
+          <h1>اشترك في النشرة الإخبارية</h1>
+          <div className="footer-form">
+            <input type="email" placeholder="البريد الإلكتروني" />
+            <div>اشترك الآن</div>
+          </div>
+        </div>
+        <div className="center"></div>
+        <div className="botttom"></div>
+      </div>
+    </div>
   )
 }
 
