b9e495125fe865016197bc70565fc6bd3df82e03
ahmedosama24 <mrahmed2466@gmail.com>
2023-10-27 16:26:06 +0200

    webpack

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,63 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import webpLoader from 'webpack-webp-loader';
+import sharp from 'sharp';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Set the desired port number
+    port: 3000,
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][ext]',
+      },
+    },
+  },
+  configureServer: {
+    // Add a configureServer block to skip WebP conversion in development mode
+    hmr: {
+      onBeforeUpdate: () => {
+        // Prevent WebP conversion in development mode
+        process.env.WEBP_SKIP = 'false';
+      },
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {},
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+                publicPath: '/',
+              },
+            },
+            {
+              loader: 'webp-loader',
+              options: {
+                quality: 75, // Adjust quality as needed
+                alphaQuality: 100,
+                method: 6, // Set encoding method (6 is a good default)
+                sharpOptions: {
+                  bypassOnDebug: true, // Skip conversion in development mode
+                  compressionLevel: 6, // Adjust compression level
+                },
+                onComplete: () => {
+                  console.log('WebP images conversion completed.');
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+});
